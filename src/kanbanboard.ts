4f8b748199461542806ed5f2a7da505901a2ca34
Florian Laabs <florian.laabs@red-crown.de>
2020-02-23 00:54:20 +0100

    - jquery ui imported

--- src/kanbanboard.ts
+++ src/kanbanboard.ts
@@ -1,12 +1,20 @@
 export interface IKanbanboard {
     lists: Array<List>;
     clickCardHandler: Function;
+    clickFooterButtonHandler: Function;
+    clickHeaderButtonHandler: Function;
+    sortCardHandler: Function;
+    sortListHandler: Function;
 }
 
 export class Kanbanboard implements IKanbanboard {
     
     lists: Array<List>;
     clickCardHandler: Function;
+    clickFooterButtonHandler: Function;
+    clickHeaderButtonHandler: Function;
+    sortCardHandler: Function;
+    sortListHandler: Function;
 
     constructor(initData: IKanbanboard){
         this.lists = new Array<List>();
@@ -16,13 +24,52 @@ export class Kanbanboard implements IKanbanboard {
         }
 
         // handler
-        $(function(){
+		$(function(){
+			$('.kanban-list-footer-button').click(function(e){
+				if(initData.clickFooterButtonHandler !== undefined) {
+					initData.clickFooterButtonHandler(e);
+				}
+			});
+			$('.kanban-item').click(function(e){
+				if(initData.clickCardHandler !== undefined) {
+					initData.clickCardHandler(e);
+				}
+			});
+			$('.kanban-list-header-button').click(function(e){
+				if(initData.clickHeaderButtonHandler !== undefined) {
+					initData.clickHeaderButtonHandler(e);
+				}
+			});
+		});
+		
+		$(function(){
+			$(".kanbanboard-list").sortable({
+				items: ".kanban-item",
+				update: function(event,ui){
+					if(initData.sortCardHandler !== undefined) {
+						initData.sortCardHandler(event,ui);
+					}
+				}
+			});
+			$(".kanban-list").disableSelection();
+			$(".kanbanboard-container").sortable({
+				items: ".kanbanboard-list",
+				handle: ".kanban-list-header",
+				update: function(event,ui){
+					if(initData.sortListHandler !== undefined) {
+						initData.sortListHandler(event,ui);
+					}
+				}
+			});
+			$(".kanbanboard-container").disableSelection();
+		});
+        /*$(function(){
             $('.kanban-item').click(function(e){
                 if(initData.clickCardHandler !== undefined) {
                     initData.clickCardHandler(e);
                 }
             });
-        });
+        });*/
 
     }
 
