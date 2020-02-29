62a5d611def37c826aa228c7c0468e49d709fb9b
Florian Laabs <florian.laabs@red-crown.de>
2020-03-01 00:04:38 +0100

    - gulp optimize

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var watchify = require('watchify');
 var tsify = require('tsify');
 var fancy_log = require('fancy-log');
 var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
 var paths = {
     pages: ['src/*.html']
 };
@@ -15,7 +16,39 @@ gulp.task('sass', function(){
       .pipe(gulp.dest('dist'))
 });
 
+gulp.task('css',function(){
+    return gulp.src('./dist/*.css')
+    .pipe(uglifycss({
+        "uglyComments":true
+    }))
+    .pipe(gulp.dest('./dist/'))
+});
+
 var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}).plugin(tsify));
+
+function bundle() {
+    return watchedBrowserify
+        .bundle()
+        .on('error', fancy_log)
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('dist'));
+}
+
+gulp.task('default',gulp.series(gulp.parallel(['sass']),gulp.parallel(['css']),bundle,(done) => {
+    
+    gulp.watch('./src/style.scss',gulp.series('sass','css'));
+    gulp.watch('./src/*.ts',gulp.series(bundle));
+
+    done();
+}));
+
+/*var watchedBrowserify = watchify(browserify({
     basedir: '.',
     debug: true,
     entries: ['src/main.ts'],
@@ -38,4 +71,4 @@ function bundle() {
 
 gulp.task('default', gulp.series(gulp.parallel(['copy-html','sass']), bundle)); // gulp.watch('./src/*.scss',gulp.series("sass"))
 watchedBrowserify.on('update', bundle);
-watchedBrowserify.on('log', fancy_log);
+watchedBrowserify.on('log', fancy_log);*/
